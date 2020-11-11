05cf99f7930fd6b76f39e74708a4d2397a06e8ca
Michael Rios <michael.rios093@gmail.com>
2020-11-11 12:34:46 -0500

    basic data layer done

--- server.js
+++ server.js
@@ -2,20 +2,41 @@ const Sequelize = require('sequelize');
 const { STRING } = Sequelize;
 const db = new Sequelize(process.env.PORT || 'postgres://postgres:JerryPine@localhost/bookmarks_app_db');
 
-const User = db.define('Users', {
-    email: {
+//making a table with sequilize and turning it into a function we 
+//can manipulate with sequileize should use soem validators going to skip 
+//for now, really just the url
+const Bookmarks = db.define('Bookmarks', {
+    name: {
+        type: STRING,
+        allowNull: false
+    },
+    //needs a validator
+    url: {
+        type: STRING,
+        allowNull: false
+    },
+    category: {
         type: STRING,
         allowNull: false
     }
 });
 
-const syncAnsSeed = async()=>{
+const syncAndSeed = async()=>{
     await db.sync({ force: true});
+    await Bookmarks.create({name: 'nypl', url: 'nypl.org', category: 'library'});
+    await Bookmarks.create({name: 'epfl', url: 'epfl.org', category: 'library'});
+    await Bookmarks.create({name: 'google', url: 'google.com', category: 'programming'});
 };
 
 const init = async() => {
+    try {
     await db.authenticate();
-    await syncAnsSeed();
+    await syncAndSeed();
+    console.log(await Bookmarks.findAll());
+    }
+    catch (ex){
+        console.log(ex);
+    }
 };
 
 
